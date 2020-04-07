064bf1e8ff1cd9d6d59a84fa0965df28c189de48
Jordan <jordanjean17@gmail.com>
2020-04-07 08:24:30 -0700

    ignore

---
+++ WeatherGift/APIKeys.swift
@@ -0,0 +1,13 @@
+//
+//  APIKeys.swift
+//  WeatherGift
+//
+//  Created by Jordan Maddox on 4/7/20.
+//  Copyright © 2020 Jordan Maddox. All rights reserved.
+//
+
+import Foundation
+
+struct APIkeys {
+    static let googlePlacesKey = "AIzaSyA0tZ3iU1uc-NWZU7bwhrp5luro_VR6gtI"
+}
---
+++ WeatherGift/WeatherLocation.swift
@@ -0,0 +1,15 @@
+//
+//  WeatherLocation.swift
+//  WeatherGift
+//
+//  Created by Jordan Maddox on 4/7/20.
+//  Copyright © 2020 Jordan Maddox. All rights reserved.
+//
+
+import Foundation
+
+struct WeatherLocation {
+    var name: String
+    var latitude: Double
+    var longitude: Double
+}
